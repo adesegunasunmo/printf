9501b6968696d90597177c93f6e7161583726932
adesegunasunmo <adesegunabayomi@gmail.com>
2022-10-18 15:48:43 -0700

    Group project

---
+++ main.h
@@ -0,0 +1,32 @@
+#ifndef mainh
+#define mainh
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * struct structprint - structure containing
+ * @q: the location and method to translate data to characters.
+ * @u: print function for specific type.
+ *
+ * Return: int
+ */
+typedef struct structprint
+{
+	char *q;
+	int (*u)(char *format, va_list);
+} struct_t;
+
+int _putchar(char ch);
+int _puts(char *string);
+int print_char(char *format, va_list);
+int print_string(char *format, va_list);
+int (*select_func(char *format))(char *format, va_list);
+int _printf(char *format, ...);
+int printint(char *format, va_list pa);
+int integer(int number);
+int count(int number);
+int _abs(int number);
+int printpercent(char *format, va_list pa);
+#endif
---
+++ print-char.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_char - prints a char.
+ * @pa: format of char.
+ * @format: format
+ * 
+ * Return: int
+ */
+int print_char(char *format, va_list v_list)
+{
+	(void)format;
+	_putchar(va_arg(v_list, int));
+	return (1);
+}
